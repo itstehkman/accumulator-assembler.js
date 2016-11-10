2e904b7405e32acef7385e14f54d581a4a1084ef
itstehkman <itstehkman@gmail.com>
2016-11-09 17:26:32 -0800

    Assembler works now, using target = (label address) - (instruction address)

--- assembler.js
+++ assembler.js
@@ -4,10 +4,6 @@ if (process.argv.length != 4) {
     console.log("usage: node assembler.js program outfile");   
 }
 
-var insts =     ["halt", "add", "sub", "and", "or", "lshift", "rshift", 
-                 "mova", "movr", "load", "store", "lui", "lli", "cmp", 
-                 "4bitstrfind", "inc", "bnci", "bnei", "blti", "bi"];
-
 var instsToOps = {
     halt:   "01000",
     add:    "01010",
@@ -34,7 +30,7 @@ var instsToOps = {
 var labelsToAddresses = {};  // to be populated while parsing
 
 var program = process.argv[2];
-var outfile = process.argv[3];
+var outfile = process.argv[3];  // make sure that outfile doesn't already exist! we'll be appending to it if it does.
 
 fs.readFile(program, "utf-8", function(err,fileData) {
     if (err) {
@@ -61,7 +57,7 @@ fs.readFile(program, "utf-8", function(err,fileData) {
     lines.forEach(function(l) {
         var parts = l.split(" ");
         if (parts[0].toLowerCase() === "label") {
-            var labelName = parts[1];
+            var labelName = parts[1].replace(":","");
             labelsToAddresses[labelName] = address;
         } else {
             address++;  // label lines don't have an address
@@ -70,6 +66,8 @@ fs.readFile(program, "utf-8", function(err,fileData) {
 
     console.log(labelsToAddresses);
     
+    // write the machine code for each line
+    address = 0;
     lines.forEach(function(l) {
         var parts = l.split(" ");
 
@@ -77,17 +75,95 @@ fs.readFile(program, "utf-8", function(err,fileData) {
             var labelName = parts[1];
             labelsToAddresses[labelName] = address;
         } else {
-            writeMachineCodeForLine(l, outfile);
-            address++;
+            writeMachineCodeForLine(l, address, outfile);
+            address++;  // label lines don't have an address
         }
 
     });
 });
 
-var writeMachineCodeForLine = function(l, outfile) {
-    var inst = l.split(" ")[0];
+var writeMachineCodeForLine = function(l, address, outfile) {
+    var parts = l.split(" ");
+    var inst = parts[0];
     if (inst === "4bitstrfind") { inst = "fbitstrfind"; }
     var op = instsToOps[inst];
-    console.log(op);
+    
+    var machineCode = op;  
+    
+    switch (inst) {
+        case "halt":    
+            machineCode += "0000"; break;
+
+        case "add": case "sub": case "and": case "or": case "lshift": case "rshift": 
+        case "mova": case "movr": case "load": case "store": case "cmp": case "fbitstrfind":
+        case "inc":
+            machineCode += regNum(parts[1]); break;
+
+        case "lui": case "lli":
+            machineCode += fourBitImm(parts[1]); break;
+
+        case "bnci": case "bnei": case "blti": case "bi":
+            machineCode += target(parts[1], address); break;
+    }
+
+    fs.appendFileSync(outfile, machineCode + "\n");
+};
+
+/*
+ * Takes a register string in the format of $rN where N is a number 0 thru 15 and
+ * returns a 4 char binary string representating that number.
+ *
+ * Anything other than this format will cause an exception.
+ */ 
+var regNum = function(s) {
+    if (s === "$ac") {
+        return "0000";
+    }
+    if (s.substring(0,2) !== "$r" || isNaN(parseInt(s.substring(2))) ) {
+        console.log("ERROR: Expected a register in the format $rN where N is a number from 0 to 15");
+        console.log("got "+s+" instead");
+        process.exit();
+    }
+    return fourBitImm(s.substring(2));
+};
+
+/*
+ * Takes a string s representing a number in decimal or hexadecimal, and returns the 
+ * binary string representation as four bit immediate.
+ */
+var fourBitImm = function(s) {
+    var n = parseInt(s).toString(2);  
+    var numZeroes = 4 - n.length;
+    if (numZeroes < 0) {
+        console.log("ERROR: Got register number not within the range 0-15");
+        process.exit();
+    }
+    return "0".repeat(numZeroes) + n;
+};
+
+/*
+ * Takes a label name and the instruction's address, and returns the target, which is equal to
+ * (label address) - (instruction address). Will be returned as a 5 bit binary string.
+ */
+var target = function(s, address) {
+    if (!labelsToAddresses.hasOwnProperty(s)) {
+        console.log("ERROR: Invalid label " + s);
+        process.exit();
+    }
+    if (address < 0) {
+        console.log("ERROR: Invalid address " + address);
+        process.exit();
+    }
+    var n = (labelsToAddresses[s] - address);
+    console.log(address + " " + n);
+
+    if (n > 0) {
+        n = parseInt(n).toString(2);
+        var numZeroes = 5 - n.length;  // guaranteed to be from 0 to 5 inclusive
+        return "0".repeat(numZeroes) + n;
+
+    } else { 
+        return ((n >>>0) & 0x1F).toString(2);  // 5 bit negative number
+    }
 };
 
